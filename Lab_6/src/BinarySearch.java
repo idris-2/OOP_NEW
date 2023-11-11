ae8deb37d6807e2c4d058046df6d0150b1b56927
Idris-2 <idris.hasanovic@stu.ibu.edu.ba>
2023-11-11 14:57:22 +0100

    fixed BinarySearch

--- Lab_6/src/BinarySearch.java
+++ Lab_6/src/BinarySearch.java
@@ -1,23 +1,21 @@
-import java.util.Arrays;
-
 class BinarySearch {
     public static boolean BinaryFind(int[] array, int value) {
-        boolean isVal = false;
-        int first = array[0];
-        int last = array[array.length - 1];
+        int left = 0;
+        int right = array.length - 1;
 
-        if(first == value){isVal = true;}
-        else if (last == value){isVal = true;}
-        int middle = array[(array.length - 1) / 2];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        while(!isVal){
-            if(array[middle] == value){isVal = true;}
-            else if(array[middle] > value){middle = middle/2;}
-            else if (array[middle] < value){middle = middle + (middle/2);}
+            // Check value in the middle
+            if (array[mid] == value) {return true;}
+
+            // If the value is greater, ignore the left half
+            if (array[mid] < value) {left = mid + 1;}
+
+            // If the value is smaller, ignore the right half
+            else {right = mid - 1;}
         }
-
-
-        Arrays.toString(array);
-        return isVal;
+        // value not present
+        return false;
     }
-}
+}       // {-1, 2, 4, 6, 8, 9, 23, 56}
